6c6646f54882906fe01470090a802528b127411d
Larry Farrell <farrelldlfarrell@gmail.com>
2014-12-02 21:37:23 -0500

    Start of bubble packing directive

--- js/directives.js
+++ js/directives.js
@@ -1,5 +1,19 @@
 angular.module('metadataViewerApp').directive('bubbleChart', function() {
     function link(scope, element, attrs) {
+        var margin = 20,
+            diameter = document.body.clientWidth;
+
+        var pack = d3.layout.pack()
+            .padding(2)
+            .size([diameter - margin, diameter - margin])
+            .value(function(d) { return d.size; });
+
+        var svg = d3.select(element[0]).append("svg")
+            .attr("width", diameter)
+            .attr("height", document.body.clientHeight)
+            .append("g")
+            .attr("transform", "translate(" + diameter / 2 + "," + diameter / 2 + ")");
+
         scope.$watch('data', function(data) {
             if (!data) { return; }
         });
