e77a731a1a98d2c1eaf4682168c74041f133adf8
Larry Farrell <farrelldlfarrell@gmail.com>
2014-12-22 20:10:41 -0500

    Text shows up in circles.

--- js/directives.js
+++ js/directives.js
@@ -5,47 +5,52 @@ angular.module('metadataViewerApp').directive('forceChart', function() {
             height = document.body.clientHeight - 50,
             color = d3.scale.category10();
 
-
-
         scope.$watch('data', function(data) {
             if(!data) { return; }
+
+            var data_nodes = { nodes: data };
+
+            var scale = d3.scale.linear()
+                .domain(d3.extent(
+                    data_nodes.nodes, function(d) { return d.count; })
+                )
+                .range([1, 75]);
+
+            var force = d3.layout.force()
+                .nodes(data_nodes.nodes)
+                .size([width, height + 175])
+                .charge(function(d) { return -d.count * .008; })
+                .start();
 
             var svg = d3.select(element[0]).append("svg")
                 .attr("width", width)
                 .attr("height", 900)
                 .call(d3.behavior.zoom().scaleExtent([1, 8]).on("zoom", zoom));
 
-            var data_nodes = { nodes: data };
+            var nodes = svg.selectAll("g")
+                .data(data_nodes.nodes);
 
-            var scale = d3.scale.linear()
-                .domain(d3.extent(data_nodes.nodes, function(d) { return d.count; } ))
-                .range([1, 75]);
+            nodes.enter().append("g").call(force.drag);
 
-            var force = d3.layout.force()
-                .nodes(data_nodes.nodes)
-                .size([width, height])
-                .charge(function(d) { return -d.count * .008; })
-                .start();
-
-            var nodes = svg.selectAll("circle")
-                .data(data_nodes.nodes)
-                .enter()
+            var circles = nodes
                 .append("circle")
                 .attr("r", function(d) { return scale(d.count); })
                 .style("fill", function(d) {
                     return color(d.type);
-                })
-                .call(force.drag);
+                });
 
-            nodes.append("text")
+               var texts= nodes.append("text")
                 .attr("dy", ".3em")
                 .style("text-anchor", "middle")
                 .style("pointer-events", "none")
-                .text(function(d) { return d.term.substring(0, d.r / 3); });
+                .text(function(d) { return d.term.substring(0, scale(d.count) / 3); });
 
             force.on("tick", function() {
-                nodes.attr("cx", function(d) { return d.x; })
-                     .attr("cy", function(d) { return d.y; });
+                circles.attr("cx", function(d) { return d.x; })
+                       .attr("cy", function(d) { return d.y; });
+
+                texts.attr("dx", function(d) { return d.x; })
+                    .attr("dy", function(d) { return d.y; });
             });
 
          /*   var zoomListener = d3.behavior.zoom()
