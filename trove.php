c1aabdf470cdec3403fb6437773abac142b5dcdd
Larry Farrell <farrelldlfarrell@gmail.com>
2014-12-28 18:42:24 -0500

    Trove updates

--- trove.php
+++ trove.php
@@ -1,12 +1,24 @@
 <?php
 include 'functions.php';
 include 'keys.php';
-
+$_GET['q'] = 'Queensland';
 $q = clean($_GET['q']);
-$path = "http://api.trove.nla.gov.au/result?key=$trove_key&zone=all&q=$q&facet=format";
+$path = "http://api.trove.nla.gov.au/result?key=$trove_key&zone=newspaper&q=$q&facet=format,year,decade,language,category";
+//echo $path; exit;
 $records = get_records($path);
 
-$data = json_decode($records, true);
+
+$data = simplexml_load_string($records);
+
 $values = array();
+
+$facets = $data->zone->facets; print_r($facets);
+foreach($facets as $facet) {
+
+        print_r($facet);
+    echo '<br><br>';
+  //  print_r($facet); exit;
+}
+//print_r($languages); exit;
 
 echo json_encode($values);
